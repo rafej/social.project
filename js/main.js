731b442abd4233adf4264797379266f545eaf93f
rafej <rafe.s@hotmail.com>
2015-11-26 18:23:08 +1100

    Redo - Init

--- js/main.js
+++ js/main.js
@@ -1,36 +1,17 @@
-var loader = document.getElementById('loader')
-  , border = document.getElementById('border')
-  , α = 0
-  , π = Math.PI
-  , i = 1;
 
-(function draw() {
-  α++;
-  α %= 360;
-  var r = ( α * π / 180 )
-    , x = Math.sin( r ) * 125
-    , y = Math.cos( r ) * - 125
-    , mid = ( α > 180 ) ? 1 : 0
-    , anim = 'M 0 0 v -125 A 125 125 1 ' 
-           + mid + ' 1 ' 
-           +  x  + ' ' 
-           +  y  + ' z';
-  //[x,y].forEach(function( d ){
-  //  d = Math.round( d * 1e3 ) / 1e3;
-  //});
- 
-  loader.setAttribute( 'd', anim );
-  border.setAttribute( 'd', anim );
-   // Redraw
-   setTimeout(draw,i) 
-}) ();
-
+var likes;
 var timer;
 function startCount() {
   timer = setInterval(count,1000);
+  likes = setInterval(count2,1000);
 }
 function count() {
   var t = document.getElementById('time');
   var currentNum = parseFloat(t.innerHTML);
-  t.innerHTML = currentNum+ 46000;
+  t.innerHTML = currentNum+46000;
+}
+function count2() {
+  var s = document.getElementById('liketext');
+  var d = parseFloat(s.innerHTML);
+  s.innerHTML = d+52000;
 }
